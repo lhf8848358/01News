edbdc1e2a71fda2d69a8fa4dc47dd7b67766645b
lhf8848358 <1994390007@qq.com>
2016-03-01 11:32:16 +0800

    first commit

---
+++ 01News/01News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01News
+//
+//  Created by 1 on 16/3/1.
+//  Copyright © 2016年 iOS 09. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01News/01News/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  01News
+//
+//  Created by 1 on 16/3/1.
+//  Copyright © 2016年 iOS 09. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
